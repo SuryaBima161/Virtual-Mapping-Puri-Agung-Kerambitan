e7a2b039e295c61d58fa8b91c173211b6efb4531
SuryaBima161 <bimaputra@student.unud.ac.id>
2024-07-06 16:08:17 +0800

    test

--- models/tb.go
+++ models/tb.go
@@ -37,6 +37,7 @@ type TbComment struct {
 	ReplyComment string    `json:"reply_comment" form:"reply_comment"`
 	TbGalery     TbGalery  `gorm:"foreignKey:Id_Galery" json:"tb_galery"`
 	Id_Galery    uuid.UUID `json:"id_galery" form:"id_galery"`
+	Status       string    `json:"status" form:"status"`
 }
 
 type TbGalery struct {
