0a538545062bdf17f44bfbdd7dbdff1de2216564
SuryaBima161 <bimaputra@student.unud.ac.id>
2024-07-30 16:57:54 +0800

    test deploy railway

--- config/config.go
+++ config/config.go
@@ -30,21 +30,24 @@ func InitDB() {
 		MYSQL_DATABASE: os.Getenv("MYSQL_DATABASE"),
 	}
 
-	fmt.Printf("Connecting to database with: %s:%s@tcp(%s:%s)/%s\n",
-		config.MYSQL_USER,
-		config.MYSQL_PASSWORD,
-		config.MYSQL_PORT,
-		config.MYSQL_HOST,
-		config.MYSQL_DATABASE,
-	)
+	// Logging variabel lingkungan untuk debugging
+	fmt.Printf("MYSQL_USER: %s\n", config.MYSQL_USER)
+	fmt.Printf("MYSQL_PASSWORD: %s\n", config.MYSQL_PASSWORD)
+	fmt.Printf("MYSQL_HOST: %s\n", config.MYSQL_HOST)
+	fmt.Printf("MYSQL_PORT: %s\n", config.MYSQL_PORT)
+	fmt.Printf("MYSQL_DATABASE: %s\n", config.MYSQL_DATABASE)
 
+	// Membuat connection string
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		config.MYSQL_USER,
 		config.MYSQL_PASSWORD,
-		config.MYSQL_PORT,
 		config.MYSQL_HOST,
+		config.MYSQL_PORT,
 		config.MYSQL_DATABASE,
 	)
+
+	// Logging connection string untuk debugging
+	fmt.Printf("Connection String: %s\n", connectionString)
 
 	var err error
 	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
@@ -54,6 +57,15 @@ func InitDB() {
 	}
 }
 
-func InitialMigration() {
-	DB.AutoMigrate(&models.TbLogin{}, &models.TbInformation{}, &models.TbComment{}, &models.TbGalery{}, &models.TbMonument{})
+func InitialMigration(db *gorm.DB) {
+	err := db.AutoMigrate(
+		&models.TbLogin{},
+		&models.TbInformation{},
+		&models.TbComment{},
+		&models.TbGalery{},
+		&models.TbMonument{},
+	)
+	if err != nil {
+		panic("Failed to migrate database: " + err.Error())
+	}
 }
--- main.go
+++ main.go
@@ -4,21 +4,41 @@ import (
 	"demonstrasi/config"
 	"demonstrasi/route"
 	"demonstrasi/util"
+	"log"
 
 	"github.com/go-playground/validator"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-// import "golang.org/x/net/route"
-func init() {
-	config.InitDB()
-	config.InitialMigration()
-	// seeder.DBSeed(config.DB)
-}
-
 func main() {
+	// Koneksi ke database
+	dsn := "root:wihpGGgSlrCkUieNGflJKzjrBWrcnPaw@tcp(monorail.proxy.rlwy.net:23451)/railway?charset=utf8&parseTime=True&loc=Local"
+	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	} else {
+		log.Println("Successfully connected to database!")
+	}
+
+	// Panggil InitialMigration untuk melakukan migrasi otomatis
+	config.InitialMigration(DB)
+	log.Println("Database migration completed!")
+
+	// Example of using DB for a query
+	var count int64
+	err = DB.Table("some_table").Count(&count).Error
+	if err != nil {
+		log.Printf("Error querying database: %v", err)
+	} else {
+		log.Printf("Count of records: %d", count)
+	}
+
 	e := route.New()
 	e.Validator = &util.CustomValidator{
 		Validator: validator.New(),
 	}
+
+	// Mulai server
 	e.Logger.Fatal(e.Start(":4040"))
 }
